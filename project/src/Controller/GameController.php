566d7c674dcfde54fef98424d6c44a28f82f7d37
ClaireCombastel <combastel.cl@gmail.com>
2021-06-23 23:41:50 +0200

    just a test to see a JSON response

--- project/src/Controller/GameController.php
+++ project/src/Controller/GameController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\JsonResponse;
 class GameController extends AbstractController
 {
     /**
@@ -16,5 +16,25 @@ class GameController extends AbstractController
         return $this->render('game/index.html.twig', [
             'controller_name' => 'GameController',
         ]);
+    }
+
+    /**
+     * @Route("/game/combat", name="game_fight")
+     */
+    public function fight(): Response
+    {
+        $response = new JsonResponse([
+            'pseudo' => "Carlos",
+            'action' => [
+                'magic' => 'water',
+                'state' => 'acceleration',
+                'defense' => 'balm'
+            ]
+        ]);
+
+        // Use the JSON_PRETTY_PRINT
+        $response->setEncodingOptions( $response->getEncodingOptions() | JSON_PRETTY_PRINT );
+
+        return $response;
     }
 }
